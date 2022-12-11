913a5715470c0a7167c3ec3a066f95794a8abada
Tyler Thornton <tyler@wtt.io>
2022-12-11 16:28:11 -0600

    Add login form

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-let button = document.querySelector("#submit");
+let postForm = document.querySelector("#postForm");
 let link = document.querySelector("#link");
 let trendingPosts = document.querySelector("#trending");
-let newPost = document.createElement("div");
 
-button.addEventListener("click", function () {
+
+postForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let newPost = document.createElement("div");
     newPost.classList.add('post');
     newPost.innerHTML = `<img src='${link.value}'>`;
+    link.value = "";
     trendingPosts.appendChild(newPost);
 });
+
+let users = {
+    "tyler": "thornton"
+}
+
+let username;
+let password;
+
+function login() {
+    username = document.querySelector("#username").value;
+    password = document.querySelector("#password").value;
+    if (username in users) {
+        if (password == users[username]) {
+            sessionStorage.setItem("username", username);
+            location.reload();
+        }
+        else {
+            window.alert("Incorrect password.");
+        }
+    }
+    else {
+        window.alert("Username not found.");
+    }
+}
+
+function logout() {
+    sessionStorage.clear();
+    location.reload();
+}
+
+let logoutButton = document.querySelector("#logoutButton");
+logoutButton.addEventListener("click", logout);
+
+let userInfo = document.querySelector("#userInfo");
+let loginForm = document.querySelector("#loginForm");
+if (sessionStorage.getItem("username") != null) {
+    userInfo.append(`Welcome, ${sessionStorage.getItem("username")}`);
+    userInfo.classList.toggle("hidden");
+    loginForm.classList.toggle("hidden");
+}
+
+loginForm.addEventListener("submit", login);
